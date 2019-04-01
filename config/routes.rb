33dd797791dc82c64948c01a8678a5b165adf052
Chi Tran <chidtran@gmail.com>
2019-04-01 17:27:13 -0600

    Adds tests for user stories 2 and 3
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :items, only: [:index]
+
+  namespace :merchants do 
+    resources :users, only: [:index]
+  end 
+
+  resources :users, only: [:new, :create]
+
+  get '/login', to: 'sessions#new'
+  get '/cart', to: 'cart#show'
 end

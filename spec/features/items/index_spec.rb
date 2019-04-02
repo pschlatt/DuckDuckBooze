c624e30085961f986f2de8843c66ab886d26f430
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-01 21:09:47 -0600

    Adds initial skeleton of item index spec

---
+++ spec/features/items/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Item Index Page', type: :feature do
+  describe 'any visitor who visits the items index page' do
+    before :each do
+      
+    end
+
+    it 'shows all items in the system except for disabled items' do
+
+    end
+
+    it 'shows the following information for each item (name, thumbnail, merchant, merchants stock, current price)' do
+
+    end
+
+    it 'the item name and item thumbnail are links to the show page of the item' do
+
+    end
+  end
+end

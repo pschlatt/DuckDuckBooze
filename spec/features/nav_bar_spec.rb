6b159db093eb9a2f5b5efea08f6e9f331c5159db
Chi Tran <chidtran@gmail.com>
2019-04-02 21:26:39 -0600

    Adds user story 3 passing tests

--- spec/features/nav_bar_spec.rb
+++ spec/features/nav_bar_spec.rb
@@ -1,74 +1,75 @@
 require 'rails_helper'
 
-RSpec.describe 'navigation bar' do 
-  context 'as a visitor' do 
-    it 'should see a navigation bar' do
-      visit root_path
+RSpec.describe 'navigation bar' do
+ context 'as a visitor' do
+   it 'should see a navigation bar with links' do
+     visit root_path
 
-      within '#nav-bar' do 
-        expect(page).to have_link('Shop Beers')
-        expect(page).to have_link('Merchants')
-        expect(page).to have_link('Cart')
-        #Next to the shopping cart link I see a count of the items in my cart
-        expect(page).to have_link('Login')
-        expect(page).to have_link('Register')
-        click_on 'Shop Beers'
-      end 
+     within '#nav-bar' do
+       expect(page).to have_link('Shop Beers')
+       expect(page).to have_link('Merchants')
+       expect(page).to have_link('Cart')
+       #Next to the shopping cart link I see a count of the items in my cart
+       expect(page).to have_link('Login')
+       expect(page).to have_link('Register')
+       click_on 'Shop Beers'
+     end
 
-      expect(current_path).to eq(items_path)
+     expect(current_path).to eq(items_path)
 
-      within '#nav-bar' do 
-        click_on 'Merchants'
-      end  
+     within '#nav-bar' do
+       click_on 'Merchants'
+     end 
 
-      expect(current_path).to eq(merchants_users_path)
+     expect(current_path).to eq(merchants_users_path)
 
-      within '#nav-bar' do 
-        click_on 'Cart'
-      end  
-      
-      expect(current_path).to eq(cart_path)
+     within '#nav-bar' do
+       click_on 'Cart'
+     end 
+    
+     expect(current_path).to eq(cart_path)
 
-      within '#nav-bar' do 
-        click_on 'Register'
-      end  
+     within '#nav-bar' do
+       click_on 'Register'
+     end 
 
-      expect(current_path).to eq(new_user_path)
+     expect(current_path).to eq(new_user_path)
 
-       within '#nav-bar' do 
-        click_on 'Cart'
-      end  
+      within '#nav-bar' do
+       click_on 'Cart'
+     end 
 
-      expect(current_path).to eq(cart_path)
-    end
-  end 
+     expect(current_path).to eq(cart_path)
+   end
+ end
 
-  context 'as a registered user' do 
-    it 'should see a navigation bar' do
-      visit root_path
+ context 'as a registered user' do
+   it 'should see a navigation bar with links' do
+     visit root_path
 
-      within '#nav-bar' do 
-        expect(page).to have_link('Shop Beers')
-        expect(page).to have_link('Merchants')
-        expect(page).to have_link('Cart')
-        #Next to the shopping cart link I see a count of the items in my cart
-        expect(page).to have_link('Login')
-        expect(page).to have_link('Register')
-        click_on 'Register'
-      end 
+     within '#nav-bar' do
+       expect(page).to have_link('Shop Beers')
+       expect(page).to have_link('Merchants')
+       expect(page).to have_link('Cart')
+       #Next to the shopping cart link I see a count of the items in my cart
+       expect(page).to have_link('Login')
+       expect(page).to have_link('Register')
+       click_on 'Register'
+     end
 
-      user = create(:user, role: 1)
-      visit login_path
+     reg_user = create(:registered_user)
+     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(reg_user)
 
-      expect(current_path).to eq(profile_path)
+     visit profile_path
 
-      within '#nav-bar' do 
-        expect(page).to_not have_link('Login')
-        expect(page).to_not have_link('Register')
-        expect(page).to have_content('Logged in as user')
-        expect(page).to have_link('Log Out')
-        click_on 'Log Out'
-      end  
-    end
-  end 
-end 
+     within '#nav-bar' do
+       expect(page).to_not have_link('Login')
+       expect(page).to_not have_link('Register')
+       expect(page).to have_link('Log Out')
+       expect(page).to have_link('Profile')
+       #expect(page).to have_content("Logged in as '#{reg_user.name}'")
+     end 
+   end
+ end
+end
+

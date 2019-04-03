d6929cf10c2a3f711eeaa746ade5275697176827
Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
2019-04-03 15:25:31 -0600

    Update merchants_controller.rb

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,7 +1,6 @@
 class Admin::MerchantsController < ApplicationController
 
   def show
-    # binding.pry
     if current_merchant? == false
       redirect_to admin_user_path(current_user)
     end

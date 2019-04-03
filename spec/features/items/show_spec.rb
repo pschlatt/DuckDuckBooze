a78a04aa5a1f85115fa075c1af2a83d24217f591
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-03 14:37:25 -0600

    Adds 'Add to Cart' button and test to item show page
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Item Show Page', type: :feature do
+  before :each do
+    @user_11 = User.create(role: 1, enabled: false, name: "Sally Shopper", street: "123 Busy Way", city: "Denver", state: "CO", zip: "80222", email: "sally@gmail.com", password: "12345678")
+    @user_12 = User.create(role: 1, enabled: false, name: "Sam Spender", street: "1 Old Street", city: "Golden", state: "CO", zip: "80403", email: "sam@gmail.com", password: "password")
+
+    @merchant_1 = User.create(role: 2, enabled: true, name: "Mike Merchant", street: "1 Old Street", city: "Golden", state: "CO", zip: "80403", email: "mike@gmail.com", password: "password")
+
+    @beer_1 = @merchant_1.items.create(name: "Heineken", description: "Pale lager, 5%", item_price: 4.00, stock: 56, enabled: true, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Heineken.jpg/156px-Heineken.jpg")
+
+    @order_1 = @user_11.orders.create(status: 0)
+    @order_2 = @user_12.orders.create(status: 0)
+
+    @order_item = OrderItem.create(item_id: @beer_1.id, order_id: @order_1.id, fulfilled: true, quantity: 12, order_price: 15.62, created_at: 2.days.ago, updated_at: 1.day.ago)
+    #class method - avg time where name = hein
+  end
+
   describe 'any visitor who visits an items show page' do
-    before :each do
-      @user_11 = User.create(role: 1, enabled: false, name: "Sally Shopper", street: "123 Busy Way", city: "Denver", state: "CO", zip: "80222", email: "sally@gmail.com", password: "12345678")
-      @user_12 = User.create(role: 1, enabled: false, name: "Sam Spender", street: "1 Old Street", city: "Golden", state: "CO", zip: "80403", email: "sam@gmail.com", password: "password")
-
-      @merchant_1 = User.create(role: 2, enabled: true, name: "Mike Merchant", street: "1 Old Street", city: "Golden", state: "CO", zip: "80403", email: "mike@gmail.com", password: "password")
-
-      @beer_1 = @merchant_1.items.create(name: "Heineken", description: "Pale lager, 5%", item_price: 4.00, stock: 56, enabled: true, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Heineken.jpg/156px-Heineken.jpg")
-
-      @order_1 = @user_11.orders.create(status: 0)
-      @order_2 = @user_12.orders.create(status: 0)
-
-      @order_item = OrderItem.create(item_id: @beer_1.id, order_id: @order_1.id, fulfilled: true, quantity: 12, order_price: 15.62, created_at: 2.days.ago, updated_at: 1.day.ago)
-      #class method - avg time where name = hein
-    end
-
     it 'shows the name, description, and large image of the item' do
 
       visit item_path(@beer_1)
@@ -36,11 +36,21 @@ RSpec.describe 'Item Show Page', type: :feature do
     end
 
     it 'shows the average amount of time for the merchant to fulfill this item' do
+
+      visit item_path(@beer_1)
+
+      within '#item-show-merchant-info' do
+        expect(page).to have_content("Average Fulfillment Time: #{@merchant_1.name}")
+      end
     end
   end
 
   describe 'as a visitor or regular user' do
-    it 'shows a link to add this item to my cart' do
+    it 'shows a button to add this item to my cart' do
+
+      visit item_path(@beer_1)
+      save_and_open_page
+      expect(page).to have_button('Add to Cart')
     end
   end
 end

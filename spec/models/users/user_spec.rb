084b5c735d42db79e33d577eacaef31c24011a2c
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-03 16:53:55 -0600

    Adds edge case to merchant avg_fill_time test
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/models/users/user_spec.rb
+++ spec/models/users/user_spec.rb
@@ -31,14 +31,17 @@ RSpec.describe User, type: :model do
        @merchant_1 = User.create(role: 2, enabled: true, name: "Mike Merchant", street: "1 Old Street", city: "Golden", state: "CO", zip: "80403", email: "mike@gmail.com", password: "password")
 
        @beer_1 = @merchant_1.items.create(name: "Heineken", description: "Pale lager, 5%", item_price: 4.00, stock: 56, enabled: true, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Heineken.jpg/156px-Heineken.jpg")
+       @beer_2 = @merchant_1.items.create(name: "Guiness", description: "Pale lager, 5%", item_price: 4.00, stock: 56, enabled: true, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Heineken.jpg/156px-Heineken.jpg")
 
        @order_1 = @user_11.orders.create(status: 0)
        @order_2 = @user_11.orders.create(status: 0)
        @order_3 = @user_11.orders.create(status: 0)
+       @order_4 = @user_11.orders.create(status: 0)
 
        @order_item_1 = OrderItem.create(item_id: @beer_1.id, order_id: @order_1.id, fulfilled: true, quantity: 3, order_price: 5.22, created_at: 2.days.ago, updated_at: 1.day.ago)
        @order_item_2 = OrderItem.create(item_id: @beer_1.id, order_id: @order_1.id, fulfilled: true, quantity: 12, order_price: 15.62, created_at: 3.days.ago, updated_at: 1.day.ago)
        @order_item_3 = OrderItem.create(item_id: @beer_1.id, order_id: @order_1.id, fulfilled: true, quantity: 24, order_price: 29.05, created_at: 4.days.ago, updated_at: 1.day.ago)
+       @order_item_4 = OrderItem.create(item_id: @beer_2.id, order_id: @order_2.id, fulfilled: true, quantity: 24, order_price: 29.05, created_at: 30.days.ago, updated_at: 1.day.ago)
 
        expect(@merchant_1.avg_fill_time(@beer_1)).to eq(48)
      end

164d6dd0ba991e137f68d7f43d43c0ff16de8542
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-04 14:30:07 -0600

    cart model total_count test passes
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart
   def initialize(cart)
     @cart = cart
   end
+
+  def total_count
+    @cart.values.sum
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Cart' do
-  it 'can calculate the total number of items it holds' do
-    cart = Cart.new({
-      "Corona" => 1,
-      "Bud Light => 2,
-      })
+  describe '.total_count' do
+    it 'can calculate the total number of items it holds' do
+      cart = Cart.new({
+        "Corona" => 1,
+        "Bud Light" => 2,
+        })
+
+      expect(cart.total_count).to eq(3)
+    end
   end
-
-  
-
 end

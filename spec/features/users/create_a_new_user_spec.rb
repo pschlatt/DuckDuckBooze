bfcfb71aae1eabb29fff2b1a95f50574731e3505
Chi Tran <chidtran@gmail.com>
2019-04-04 11:22:00 -0600

    Completes user story 8 - all tests passing, closes #72
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/users/create_a_new_user_spec.rb
+++ spec/features/users/create_a_new_user_spec.rb
@@ -26,5 +26,34 @@ RSpec.describe 'User Registration Form' do
       expect(page).to have_content("Welcome, #{new_user.name}!")
       expect(page).to have_content("You are now registered and logged in.")
     end 
+
+    it 'cannot create an account without all required fields' do 
+      visit new_user_path
+
+      fill_in 'Name', with: 'Chicken'
+      fill_in 'Password', with: 'Dinner'
+      fill_in 'Confirm Password', with: 'Dinner'
+      fill_in 'Street', with: '123 Dead End'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'Colorado'
+      fill_in 'Zip', with: '88888'
+
+      click_button 'Create User'
+
+      expect(page).to have_content("CREATE AN ACCOUNT")
+      expect(page).to have_content('Missing required field(s). Please try again.')
+
+      fill_in 'Name', with: 'Chicken'
+      fill_in 'Password', with: 'Dinner'
+      fill_in 'Street', with: '123 Dead End'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'Colorado'
+      fill_in 'Zip', with: '88888'
+
+      click_button 'Create User'
+
+      expect(page).to have_content("CREATE AN ACCOUNT")
+      expect(page).to have_content('Missing required field(s). Please try again.')
+    end 
   end 
 end 

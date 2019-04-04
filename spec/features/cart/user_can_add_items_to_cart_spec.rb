d8ccb1542e0cf4677991de54ee03083b48261a7e
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-04 15:46:40 -0600

    Removes redundant testing
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/cart/user_can_add_items_to_cart_spec.rb
+++ spec/features/cart/user_can_add_items_to_cart_spec.rb
@@ -32,30 +32,6 @@ RSpec.describe 'a visitor or registered user' do
         expect(page).to have_content("Cart: 2")
       end
 
-      it 'as a visitor - this item is added to my cart, I am redirected to the items catalogue' do
-
-        visit item_path(@item)
-
-        click_on("Add to Cart")
-
-        expect(current_path).to eq(items_path)
-      end
-
-      it 'as a visitor - it shows a flash confirmation message, and my cart in the nav bar increments' do
-
-        visit item_path(@item)
-
-        click_on("Add to Cart")
-
-        expect(page).to have_content("You have added #{@item.name} to your cart")
-        expect(page).to have_content("Cart: 1")
-
-        visit item_path(@item)
-        click_on("Add to Cart")
-
-        expect(page).to have_content("You have added #{@item.name} to your cart")
-        expect(page).to have_content("Cart: 2")
-      end
 
       it 'as a registered user - this item is added to my cart, I am redirected to the items catalogue' do
         user = create(:user)
@@ -90,12 +66,3 @@ RSpec.describe 'a visitor or registered user' do
     end
   end
 end
-
-
-# As a visitor or registered user
-# When I visit an item's show page from the items catalog
-# I see a link or button to add this item to my cart
-# And I click its link or button
-# I am returned to the item index page
-# I see a flash message indicating the item has been added to my cart
-# The navigation bar increments my cart counter

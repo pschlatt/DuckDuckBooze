82aa0b32fa46f83ea21ac21556bd96403e65e15d
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-04 15:40:03 -0600

    Completes user story 20
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/cart/user_can_add_items_to_cart_spec.rb
+++ spec/features/cart/user_can_add_items_to_cart_spec.rb
@@ -32,22 +32,60 @@ RSpec.describe 'a visitor or registered user' do
         expect(page).to have_content("Cart: 2")
       end
 
-
-
-
-
-
-
-
-      xit 'as a registered user this item is added to my cart, I am redirected to the items catalogue' do
-
-        user = create(:user)
+      it 'as a visitor - this item is added to my cart, I am redirected to the items catalogue' do
 
         visit item_path(@item)
 
         click_on("Add to Cart")
 
         expect(current_path).to eq(items_path)
+      end
+
+      it 'as a visitor - it shows a flash confirmation message, and my cart in the nav bar increments' do
+
+        visit item_path(@item)
+
+        click_on("Add to Cart")
+
+        expect(page).to have_content("You have added #{@item.name} to your cart")
+        expect(page).to have_content("Cart: 1")
+
+        visit item_path(@item)
+        click_on("Add to Cart")
+
+        expect(page).to have_content("You have added #{@item.name} to your cart")
+        expect(page).to have_content("Cart: 2")
+      end
+
+      it 'as a registered user - this item is added to my cart, I am redirected to the items catalogue' do
+        user = create(:user)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit item_path(@item)
+
+        click_on("Add to Cart")
+
+        expect(current_path).to eq(items_path)
+      end
+
+      it 'as a registered user - it shows a flash confirmation message, and my cart in the nav bar increments' do
+        user = create(:user)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit item_path(@item)
+
+        click_on("Add to Cart")
+
+        expect(page).to have_content("You have added #{@item.name} to your cart")
+        expect(page).to have_content("Cart: 1")
+
+        visit item_path(@item)
+        click_on("Add to Cart")
+
+        expect(page).to have_content("You have added #{@item.name} to your cart")
+        expect(page).to have_content("Cart: 2")
       end
     end
   end

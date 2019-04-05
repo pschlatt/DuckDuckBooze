ef62ce67a5d968f2b69f835e7c656983184ead2d
earl-stephens <stephensearl@hotmail.com>
2019-04-04 22:46:00 -0600

    completed user story 55

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,18 @@
 class Admin::UsersController < ApplicationController
+  before_action :check_admin_status
 
   def index
     @users = User.where(role: 1)
   end
 
   def show
+  end
+
+  private
+
+  def check_admin_status
+    render file: "/public/404", status: 404 unless current_admin?
+
   end
 
 end
--- spec/features/admin/admin_users_index_spec.rb
+++ spec/features/admin/admin_users_index_spec.rb
@@ -1,12 +1,5 @@
 require "rails_helper"
-# As an admin user
-# When I click a new "Users" link in the nav (only visible to admins)
-# Then my current URI route is "/admin/users"
-# Only admin users can reach this path.
-# I see all users in the system who are not merchants nor admins.
-# Each user's name is a link to a show page for that user ("/admin/users/5")
-# Next to each user's name is the date they registered
-# Next to each user's name is a button that says 'Upgrade to Merchant'
+
 describe "as an admin" do
   describe "when I go to users link" do
     it "shows all regular users" do
@@ -17,7 +10,7 @@ describe "as an admin" do
       merchant = create(:merchant)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       visit admin_users_path
-save_and_open_page
+
       within ".user-#{user_1.id}-section" do
         expect(page).to have_content(user_1.name)
         expect(page).to have_content(user_1.created_at.to_s(:long))
@@ -37,6 +30,42 @@ save_and_open_page
         expect(page).to have_link("Upgrade to Merchant")
       end
       expect(page).to_not have_content(merchant.name)
+    end
+
+    it "won't let merchants see Users link" do
+      merchant = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+
+      expect(page).to_not have_link("Users")
+
+      visit admin_users_path
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end
+    it "won't let registered users see Users link" do
+      user_1 = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      expect(page).to_not have_link("Users")
+
+      visit admin_users_path
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end
+
+    it "won't let visitors see Users link" do
+
+      visit root_path
+
+      expect(page).to_not have_link("Users")
+
+      visit admin_users_path
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end
   end
 end

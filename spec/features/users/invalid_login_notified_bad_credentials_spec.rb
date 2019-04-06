56822136a17a445cad503643ae48c9eb92bcb87c
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-05 18:17:54 -0600

    Adds full testing and functionality for invalid log in info
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/users/invalid_login_notified_bad_credentials_spec.rb
+++ spec/features/users/invalid_login_notified_bad_credentials_spec.rb
@@ -9,13 +9,30 @@ RSpec.describe 'as a visitor attempting to log in - login path' do
 
   context 'if I submit invalid information' do
     it 'notifies me that my credentials were incorrect - but I am not told which ones - and I am redirected back to log in' do
-      fill_in 'Email', with: @reg_user
+      fill_in 'Email', with: @reg_user.email
       fill_in 'Password', with: 'incorrect'
 
       click_on "Submit"
-      
+
       expect(page).to have_content("Invalid Credentials")
       expect(current_path).to eq(login_path)
+
+      fill_in 'Email', with: 'incorrect@email.com'
+      fill_in 'Password', with: @reg_user.password
+
+      click_on "Submit"
+
+      expect(page).to have_content("Invalid Credentials")
+      expect(current_path).to eq(login_path)
+
+      fill_in 'Email', with: @reg_user.email
+      fill_in 'Password', with: @reg_user.password
+
+      click_on "Submit"
+
+      expect(page).to_not have_content("Invalid Credentials")
+      expect(current_path).to_not eq(login_path)
+      expect(current_path).to eq(profile_path)
     end
   end
 end

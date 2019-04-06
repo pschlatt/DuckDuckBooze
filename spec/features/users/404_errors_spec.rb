a4d4c0e0940777e2ed6407afe8fbfc0dab9c4377
Chi Tran <chidtran@gmail.com>
2019-04-06 15:53:27 -0600

    Adds merchant 404 messages for /admin /cart /profile paths
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/users/404_errors_spec.rb
+++ spec/features/users/404_errors_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe '404 errors' do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end 
 
-    it 'to any /admin' do 
+    it 'to any /admin path' do 
       visit '/admin/users'
 
       expect(page.status_code).to eq(404)
@@ -39,6 +39,9 @@ RSpec.describe '404 errors' do
 
   describe 'when a registered user tries to navigate' do
     it 'to any /dashboard path' do 
+      reg_user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(reg_user)
+
       visit '/dashboard'
 
       expect(page.status_code).to eq(404)
@@ -55,7 +58,10 @@ RSpec.describe '404 errors' do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
     end 
 
-    it 'to any /admin' do 
+    it 'to any /admin path' do 
+      reg_user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(reg_user)
+
       visit '/admin/users'
 
       expect(page.status_code).to eq(404)
@@ -72,6 +78,52 @@ RSpec.describe '404 errors' do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
 
       visit '/admin/dashboard'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end 
+  end
+  
+  describe 'when a merchant tries to navigate' do
+    it 'to any /profile path' do 
+      merchant = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+
+      visit '/profile'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/profile/orders'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end 
+
+    it 'to any /admin path' do 
+      visit '/admin/users'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/admin/users/1'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/admin/merchants/1'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/admin/dashboard'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end 
+
+    it 'to any /cart path' do 
+      visit '/cart'
 
       expect(page.status_code).to eq(404)
       expect(page).to have_content("The page you were looking for doesn't exist (404)")

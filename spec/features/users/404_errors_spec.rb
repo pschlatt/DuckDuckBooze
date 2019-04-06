527e9ec25250d2c7698a4cd43f885795b9ad01a6
Chi Tran <chidtran@gmail.com>
2019-04-06 16:02:39 -0600

    Adds admin 404 msgs /profile /dashboard /cart
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/users/404_errors_spec.rb
+++ spec/features/users/404_errors_spec.rb
@@ -38,10 +38,12 @@ RSpec.describe '404 errors' do
   end 
 
   describe 'when a registered user tries to navigate' do
-    it 'to any /dashboard path' do 
-      reg_user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(reg_user)
+    before :each do 
+      @reg_user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@reg_user)
+    end 
 
+    it 'to any /dashboard path' do 
       visit '/dashboard'
 
       expect(page.status_code).to eq(404)
@@ -85,10 +87,12 @@ RSpec.describe '404 errors' do
   end
   
   describe 'when a merchant tries to navigate' do
-    it 'to any /profile path' do 
-      merchant = create(:merchant)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+    before :each do 
+      @merchant = create(:merchant)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
+    end 
 
+    it 'to any /profile path' do 
       visit '/profile'
 
       expect(page.status_code).to eq(404)
@@ -117,6 +121,49 @@ RSpec.describe '404 errors' do
       expect(page).to have_content("The page you were looking for doesn't exist (404)")
 
       visit '/admin/dashboard'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end 
+
+    it 'to any /cart path' do 
+      visit '/cart'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end 
+  end 
+
+  describe 'when an admin tries to navigate' do
+    before :each do
+      @admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+    end 
+    it 'to any /profile path' do 
+      visit '/profile'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/profile/orders'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+    end 
+
+    it 'to any /dashboard path' do 
+      visit '/dashboard'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/dashboard/items'
+
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content("The page you were looking for doesn't exist (404)")
+
+      visit '/dashboard/items/1'
 
       expect(page.status_code).to eq(404)
       expect(page).to have_content("The page you were looking for doesn't exist (404)")

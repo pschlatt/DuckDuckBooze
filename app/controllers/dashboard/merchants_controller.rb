5306ab13ace31a58a10f63b6fe6fa78558962417
Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
2019-04-06 11:30:45 -0600

    Update merchants_controller.rb

--- app/controllers/dashboard/merchants_controller.rb
+++ app/controllers/dashboard/merchants_controller.rb
@@ -1,6 +1,13 @@
 class Dashboard::MerchantsController < ApplicationController
+  before_action :check_merchant_status
 
   def show
     @merchant = current_user
+  end
+
+  private
+
+  def check_merchant_status
+    render file: "/public/404", status: 404 unless current_merchant?
   end
 end

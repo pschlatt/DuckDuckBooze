421408f1d528e3571b043dffdb14918c6fa88093
earl-stephens <stephensearl@hotmail.com>
2019-04-06 17:39:55 -0600

    saving changes before switching branches

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,8 +1,12 @@
 class Users::OrdersController < ApplicationController
 
   def index
+    # binding.pry
+    if current_user
+      @orders = current_user.orders
+    end
   end
-    
+
   def show
   end
 
--- spec/features/users/order_show_spec.rb
+++ spec/features/users/order_show_spec.rb
@@ -15,22 +15,24 @@ describe 'the user profile orders page' do
     beer_1 = Item.create!(name: "MGD", description: "good beer", stock: 12, item_price: 1.5, user_id: merchant.id )
     beer_2 = Item.create!(name: "Mich Ultra", description: "better beer", stock: 15, item_price: 2.5, user_id: merchant.id )
     beer_3 = Item.create!(name: "4 Noses", description: "yummy beer", stock: 18, item_price: 3.5, user_id: merchant.id )
-    order_1 = user.orders.create!(created_at: 3.days.ago)
-    order_2 = user.orders.create!(created_at: 2.days.ago)
+    order_1 = user.orders.create!(created_at: 3.days.ago, updated_at: 1.day.ago)
+    order_2 = user.orders.create!(created_at: 2.days.ago, updated_at: 1.day.ago)
+    # order_3 = user.orders.create!(created_at: 4.days.ago)
     oi_1 = OrderItem.create!(fulfilled: false, quantity: 3, order_price: 3, order_id: order_1.id, item_id: beer_1.id, updated_at: 1.day.ago)
     oi_2 = OrderItem.create!(fulfilled: false, quantity: 6, order_price: 2, order_id: order_2.id, item_id: beer_2.id, updated_at: 1.day.ago)
-
+    oi_3 = OrderItem.create!(fulfilled: false, quantity: 9, order_price: 4, order_id: order_2.id, item_id: beer_3.id, updated_at: 1.day.ago)
+# binding.pry
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit profile_orders_path
-
+binding.pry
     within ".info-#{order_1.id}" do
-      expect(page).to have_link("#{order_1.id}")
-      expect(page).to have_content("#{order_1.created_at}")
-      expect(page).to have_content("#{oi_1.updated_at}")
-      expect(page).to have_content("#{oi_1.status}")
-      expect(page).to have_content("#{oi_1.quan}")
-      expect(page).to have_content("#{oi_1.status}")
+      expect(page).to have_link("Order ID: #{order_1.id}")
+      expect(page).to have_content("Created at: #{order_1.created_at.to_s(:long)}")
+      expect(page).to have_content("Last updated: #{order_1.updated_at.to_s(:long)}")
+      expect(page).to have_content("Order status: #{order_1.status}")
+      # expect(page).to have_content("#{oi_1.quan}")
+      # expect(page).to have_content("#{oi_1.status}")
     end
   end
 end

71b14b7906fd9f9bbfdb77701800d7e698dd729d
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-08 11:25:13 -0600

    Adds fix for user password
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/users/merchants/index_spec.rb
+++ spec/features/users/merchants/index_spec.rb
@@ -25,4 +25,47 @@ RSpec.describe 'merchant index page' do
       end
     end
   end
+
+  context 'merchant index page shows statistics' do
+    before :each do
+      @user = create(:user)
+
+      @merch_1 = create(:merchant)
+      @merch_2 = create(:merchant)
+      @merch_3 = create(:merchant)
+      @merch_4 = create(:merchant)
+
+      @beer_1 = Item.create!(name: "MGD", description: "good beer", stock: 12, item_price: 1.5, user_id: @merch_1.id )
+      @beer_2 = Item.create!(name: "Mich Ultra", description: "better beer", stock: 15, item_price: 2.5, user_id: @merch_2.id )
+      @beer_3 = Item.create!(name: "4 Noses", description: "yummy beer", stock: 18, item_price: 3.5, user_id: @merch_3.id )
+      @beer_4 = Item.create!(name: "Guiness", description: "the best", stock: 18, item_price: 3.5, user_id: @merch_4.id )
+
+      order_1 = user.orders.create!(created_at: 3.days.ago, updated_at: 1.day.ago)
+      order_2 = user.orders.create!(created_at: 2.days.ago, updated_at: 1.day.ago)
+
+
+
+
+
+      oi_1 = OrderItem.create!(fulfilled: false, quantity: 3, order_price: 3, order_id: order_1.id, item_id: beer_1.id, updated_at: 1.day.ago)
+      oi_2 = OrderItem.create!(fulfilled: false, quantity: 6, order_price: 2, order_id: order_2.id, item_id: beer_2.id, updated_at: 1.day.ago)
+      oi_3 = OrderItem.create!(fulfilled: false, quantity: 9, order_price: 4, order_id: order_2.id, item_id: beer_3.id, updated_at: 1.day.ago)
+
+
+
+
+
+
+    end
+
+
+
+
+
+
+
+
+
+
+  end
 end
--- spec/features/users/registered_users/user_can_edit_their_profile_spec.rb
+++ spec/features/users/registered_users/user_can_edit_their_profile_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'registered user' do
   describe 'can edit their profile data' do
-    before :each do 
+    before :each do
       @user = create(:user)
       @user_2 = create(:user, email: 'email@gmail.com')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    end 
+    end
 
     it 'via a link on their profile page' do
       visit 'profile'
 
       click_link('Edit Profile')
       expect(current_path).to eq('/profile/edit')
-    end 
+    end
 
     it 'sees a form to edit on /profile/edit' do
       visit 'profile/edit'
-      
+
       within '#edit-user-form' do
         expect(page).to have_content('Name')
         expect(page).to have_content('Email')
@@ -27,31 +27,32 @@ RSpec.describe 'registered user' do
         expect(page).to have_content('City')
         expect(page).to have_content('State')
         expect(page).to have_content('Zip')
-      end 
-    end 
+      end
+    end
 
     it 'can edit successfully' do
       visit '/profile/edit'
-      
+
       within '#edit-user-form' do
         fill_in 'Name', with: "Blah"
         click_on('Save User')
-      end 
-      
-      expect(@user.name).to eq("Blah")
+      end
+
+      expect(current_path).to eq(profile_path)
+      expect(page).to have_content("Blah")
       expect(page).to have_content('You have updated your profile')
-    end 
+    end
 
     it 'cannot edit without unique email' do
       visit '/profile/edit'
-      
+
       within '#edit-user-form' do
         fill_in 'Email', with: "email@gmail.com"
         click_on('Save User')
-      end 
+      end
 
       expect(current_path).to eq('/profile/edit')
       expect(page).to have_content('Email has already been taken')
-    end 
-  end 
-end 
+    end
+  end
+end

4be4900498a41d86b64c851fc393d2ec0ab1b808
Chi Tran <44073660+chitasan@users.noreply.github.com>
2019-04-08 09:53:10 -0600

    Update admin_user_show_spec.rb

--- spec/features/admin/admin_user_show_spec.rb
+++ spec/features/admin/admin_user_show_spec.rb
@@ -1,17 +1,5 @@
 require "rails_helper"
-# As an admin user
-# When I visit a user's profile page ("/admin/users/5")
-# I see a link to "upgrade" the user's account to become
-# a merchant
-# When I click on that link
-# I am redirected to ("/admin/merchants/5") because the
-# user is now a merchant
-# And I see a flash message indicating the user has been
-# upgraded
-# The next time this user logs in they are now a
-# merchant
-# Only admins can reach any route necessary to
-# upgrade the user to merchant status
+
 describe 'when admin visits users profile page' do
   it 'lets admin upgrade user to merchant' do
     admin = create(:admin)
@@ -23,10 +11,8 @@ describe 'when admin visits users profile page' do
     click_link "Upgrade"
     user.update(role: 2)
     new_merch = user
-    # save_and_open_page
+ 
     expect(current_path).to eq(admin_merchant_path(new_merch))
     expect(page).to have_content("User has been upgraded to a merchant")
-    # save_and_open_page
-
   end
 end

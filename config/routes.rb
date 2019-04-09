bbfc851fe6748fdd0805838aad84384bc43fab98
Chi Tran <chidtran@gmail.com>
2019-04-08 19:19:12 -0600

    Completes user stories 42 and 43 disable enable merchant
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 
   namespace :admin do
-    resources :merchants, only: [:show, :update]
+    resources :merchants, only: [:show, :update, :index]
     resources :users, only: [:show, :index, :update, :new]
     get '/dashboard', to: 'dashboard#show'
   end

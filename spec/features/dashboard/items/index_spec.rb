413a30ffe5bfa7bc36c1a5d2552a02df6199ca78
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-09 12:15:46 -0600

    Adds test set up for user story 44
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-authored-by: Chi Tran <44073660+chitasan@users.noreply.github.com>
    Co-authored-by: Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>

--- spec/features/dashboard/items/index_spec.rb
+++ spec/features/dashboard/items/index_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Items Index Page', type: :feature do
-  context 'as a merchant visiting my items page - when I click an enable button next to an item' do
+  describe 'as a merchant visiting my items page' do
     before :each do
 
       @merch = create(:merchant)
-      #
-      # @item_1 = create(:item, user_id: @merch.id, stock: 75)
-      # @item_2 = create(:item, user_id: @merch.id, stock: 85)
-      # @item_3 = create(:item, user_id: @merch.id, stock: 95)
+      @user = create(:user)
 
-      # @order_2 = create(:order, user_id: @user_1.id)
+      @item_1 = create(:item, user_id: @merch.id, stock: 75)
+      @item_2 = create(:item, user_id: @merch.id, stock: 85)
+      @item_3 = create(:item, user_id: @merch.id, stock: 95)
+      @item_4 = create(:item, user_id: @merch.id, stock: 105)
+
+      @order_1 = create(:order, user_id: @user.id)
       # @order_3 = create(:order, user_id: @user_2.id)
       # @order_3 = create(:order, status: 'shipped', user_id: @user_2.id)
       # @order_4 = create(:shipped_order, user_id: @user_2.id)
@@ -23,12 +25,39 @@ RSpec.describe 'Merchant Items Index Page', type: :feature do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merch)
 
       visit dashboard_items_path
-
     end
 
-    it 'return me to my items page, I see a confirmation message, and I see that the item is now enabled for sale' do
+    it 'shows a link to add new items to the system' do
+
+      expect(page).to have_link("Add New Item")
+    end
+
+    it 'shows all items that I have added to the system including - id, name, image, price, inventory, and edit link' do
+      # save_and_open_page
+      within "#item-#{@item_1.id}" do
+        expect(page).to have_content("ID: #{@item_1.id}")
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content("Price: #{@item_1.item_price}")
+        expect(page).to have_content("Stock: #{@item_1.stock}")
+        # expect(page).to have_xpath("//img[@src='#{@item_1.image}']")
+        expect(page).to have_link("Edit")
+      end
+
+      within "#item-#{@item_2.id}" do
+        expect(page).to have_content("ID: #{@item_2.id}")
+        expect(page).to have_content(@item_2.name)
+        expect(page).to have_content("Price: #{@item_2.item_price}")
+        expect(page).to have_content("Stock: #{@item_2.stock}")
+        # expect(page).to have_xpath("//img[@src='#{@item_1.image}']"2
+        expect(page).to have_link("Edit")
+      end
+    end
+
+    context'when I click an enable button next to an item' do
+      xit 'return me to my items page, I see a confirmation message, and I see that the item is now enabled for sale' do
 
 
+      end
     end
   end
 end
